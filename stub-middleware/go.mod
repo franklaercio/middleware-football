d65fb5ab6730f0da0d59dfeca826dd3aa081733a
Frank <franklaercio@gmail.com>
2024-04-22 05:59:30 -0300

    feat: adding stub to get connection

--- stub-middleware/go.mod
+++ stub-middleware/go.mod
@@ -1,1 +1,3 @@
-module stub
+module github.com/franklaercio/middleware-football/stub-middleware
+
+go 1.22.2
--- stub-middleware/main.go
+++ stub-middleware/main.go
@@ -1,7 +1,49 @@
-package main
+package stub
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
 
-func main() {
-	fmt.Println("Hello World!")
+type Stub struct {
+	Message string
+}
+
+func (s *Stub) GetStubFromService() error {
+	conn, err := net.Dial("udp", "127.0.0.1:8080")
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	conn.Write([]byte(fmt.Sprintf("Get|ServiceFootball")))
+
+	buf := make([]byte, 1024)
+
+	n, err := conn.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	addr := string(buf[:n])
+
+	conn, err = net.Dial("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	conn.Write([]byte("Get|CurrentRound|PL|2021"))
+
+	buf = make([]byte, 1024)
+
+	n, err = conn.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	s.Message = string(buf[:n])
+	return nil
 }
